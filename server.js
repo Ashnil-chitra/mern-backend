52a5f141ff100b54bff89cefb4a228d650eadc00
Ashnil-chitra <ashnil@proprofs.com>
2025-07-19 16:37:08 +0530

    Added contact form backend

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const contactRoute = require("./routes/contact");
 
 require('dotenv').config();
 
@@ -19,6 +20,8 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 .catch((err) => console.log(err));
 
 app.use('/api/users', require('./routes/userRoutes'));
+
+app.use("/api/contact", contactRoute);
 
 app.get("/", (req, res) => {
   res.send("Backend is running!");
